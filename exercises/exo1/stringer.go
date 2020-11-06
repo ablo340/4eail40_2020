6de88433e20ea4bf00011857e9e9db42412356ed
ablo340 <ablo110@live.fr>
2020-11-06 17:10:01 +0100

    first commit for exo1: implementation of fmt.stringer

--- exercises/exo1/stringer.go
+++ exercises/exo1/stringer.go
@@ -3,6 +3,26 @@ package main
 import "fmt"
 
 // Implement types Rectangle, Circle and Shape
+type Rectangle struct {
+	Length float64
+	Width float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Shape interface {
+	fmt.Stringer
+}
+
+func (r *Rectangle) String() string{
+	return fmt.Sprintf("Rectangle of width %f and length %f", r.Width, r.Length)
+}
+
+func (c *Circle) String() string{
+	return fmt.Sprintf("Circle of radius %f", c.Radius)
+}
 
 func main() {
 	r := &Rectangle{2, 3}
